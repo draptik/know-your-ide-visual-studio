6af5daefcffaf376436d79f9a4f992a96a51c236
Patrick Drechsler <patrick@pdrechsler.de>
2017-10-18 21:51:32 +0200

    updated code sample using fizzbuzz

---
+++ code/Demo/Demo/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo
+{
+    public class FizzBuzz
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public string Parse(int i)
+        {
+            //var unused = 1;
+
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (i % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return i.ToString();
+        }
+    }
+}
---
+++ code/Demo/Demo/FizzBuzzTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Demo
+{
+    public class FizzBuzzTests
+    {
+        private readonly FizzBuzz _sut;
+
+        public FizzBuzzTests()
+        {
+            _sut = new FizzBuzz();
+        }
+
+        [Fact]
+        public void Adding_2_numbers_returns_correct_result()
+        {
+            _sut.Add(1, 1).Should().Be(2);
+        }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        public void FizzBuzz_should_return_correct_result(int input, string expectedResult)
+        {
+            _sut.Parse(input).Should().Be(expectedResult);
+        }
+    }
+}
